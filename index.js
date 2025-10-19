1c4b20fc052a3f06acc13cd890b45c2c9ec556c6
TechCrent <ciicicrentsil@gmail.com>
2025-10-19 19:36:21 +0000

    JS: Fixed variable inconsistency

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ const row6 = document.querySelectorAll('.sixth-row .box');
 const WORD_URL = "https://words.dev-apis.com/word-of-the-day";
 const VALID_URL = "https://words.dev-apis.com/validate-word";
 
+let currentLetters = [];
+let guessWord = "";
+let wordOfTheDay = "";
+
 const boxes = [row1,row2,row3,row4,row5,row6]
 let currentRow = 0;
 let currentBox = 0;
@@ -22,10 +26,16 @@ let currentBox = 0;
 //API Handling // Getting Word of the day
 async function getWord(){
     //First API (Get) Getting word of the Day
-    const promise = await fetch(WORD_URL);
+    const promise = await fetch(WORD_URL,{
+            headers:{
+                "Content-Type": "application/json",
+            },
+    }
+    );
     const ProcessedResponse = await promise.json();
-    const wordOfTheDay = ProcessedResponse.word;
+    wordOfTheDay = ProcessedResponse.word.toLowerCase();
 
+    return wordOfTheDay;
 }
 
 
@@ -36,6 +46,7 @@ function initiate(){
         .addEventListener("keydown",(event)=>{
            if(isLetter(event.key) && currentBox < 5){
                 boxes[currentRow][currentBox].innerText = event.key.toUpperCase();
+                currentLetters.push(event.key.toUpperCase());
                 currentBox++;
            }else{
 
@@ -55,6 +66,7 @@ function Del(){
             if(currentBox === 0 && event.key === "Backspace"){
                 currentBox = 0;
             }else if(currentBox <= 5 && event.key === "Backspace"){
+                currentLetters.pop();
                 currentBox--;
                 boxes[currentRow][currentBox].innerText = null;
             };
@@ -64,12 +76,21 @@ function Del(){
 //Enter handling & Row Switch handling
 function Enter(){
     document
-        .addEventListener("keydown",(event)=>{
+        .addEventListener("keydown",async (event)=>{
             if(currentRow === 5 && currentBox === 5 && event.key === "Enter"){
                 alert('This is the end!')
             }else if(currentBox === 5 && event.key === "Enter"){
+                guessWord = currentLetters.join("").toLowerCase();
+                console.log(guessWord);
+
+                const validWord = await validateWord(guessWord);
+
+
+                ToF(validWord)
+                currentLetters = [];
                 currentRow ++;
                 currentBox = 0;
+
             }
         });
 }
@@ -77,23 +98,30 @@ function Enter(){
 
 
 //API Handling // Is-Word Validation
-async function validateWord(){
+async function validateWord(guessWord){
     //Second API(Post) Validating if its a word
     const promise2 = await fetch(VALID_URL ,{
         method: "POST",
         headers:{
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(/*Guessed word should be here*/),
+        body: JSON.stringify({ word: guessWord}),
     });
     const ProcessedResponse2 = await promise2.json()
-
+    return ProcessedResponse2.validWord;
+}
     //Getting True or False Statement
-    const validWord = ProcessedResponse2.validWord;
+function ToF(validWord){
+    if(!validWord){
+        alert("Not a real Word!")
+    }else if(guessWord === wordOfTheDay){
+        alert("Correct answer")
+    }else{
+        alert("Wrong word, try again.")
+    }
 }
 
-//Word Validation(Word OF The Day or Not)
-
+getWord();
 initiate();
 Del();
 Enter();
